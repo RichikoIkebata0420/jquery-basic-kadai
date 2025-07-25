966e8f785d785903e5d17ab91f65001d152ed6c9
RichikoIkebata0420 <79066583+RichikoIkebata0420@users.noreply.github.com>
2025-07-26 03:11:58 +0900

    jQuery課題「様々な形で文字表示」

---
+++ kadai_010/summary.js
@@ -0,0 +1,19 @@
+//色変更
+$('#change-color').on('click',function(){
+    $('#target').css('color', 'red');
+});
+
+//文字変更
+$('#change-text').on('click',function(){
+    $('#target').text('Hello!');
+});
+
+//フェードアウト
+$('#fade-out').on('click',function(){
+    $('#target').fadeOut();
+});
+
+//フェードイン
+$('#fade-in').on('click',function(){
+    $('#target').fadeIn();
+});
